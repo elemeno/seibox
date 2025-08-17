0856107f00f1f6ff3c8c3cb3838df4c97146255b
erustin <ed@well.com>
2025-08-17 08:58:34 +0100

    Test Black formatting hook (formatted)

---
+++ test_formatting.py
@@ -0,0 +1,12 @@
+def badly_formatted_function(a, b, c):
+    """This function has intentionally bad formatting."""
+    result = a + b + c
+    return result
+
+
+class BadlyFormattedClass:
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
